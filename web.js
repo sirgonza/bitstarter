017f0e878f5ca78a7dc328ff57b19db7fdbe943b
Raul <rgonzalezpunzano@gmail.com>
2013-07-13 20:57:18 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,24 @@
 var express = require('express');
+//var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+/*  
+fs.readFile('./index.html, function (err, data) {
+  if (err) throw err;
+
+  //var snowman = new Buffer("☃", "utf-8")
+
+  console.log(data);
+});
+
+
+*/
+
+
+response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
